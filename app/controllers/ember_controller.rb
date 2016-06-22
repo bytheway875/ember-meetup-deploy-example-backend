8ec50e2c03402d6d43a8ad2b007fe9e97f2b2cda
Shannon <s.byrne2254@gmail.com>
2016-06-21 22:07:02 -0500

    remove references to revisions for simplicity

--- app/controllers/ember_controller.rb
+++ app/controllers/ember_controller.rb
@@ -1,25 +1,17 @@
 # https://github.com/ghedamat/ember-deploy-demo/blob/master/edd-rails/app/controllers/demo_controller.rb
 class EmberController < ApplicationController
-  SHORT_UUID_V4_REGEXP = /\A[0-9a-f]{7}\z/i
 
   def index
     index_key = if Rails.env.development?
                   'ember-meetup:index:__development__'
-                elsif fetch_revision
-                  "ember-meetup:index:#{fetch_revision}"
                 else
-                  Sidekiq.redis { |r| "edd-cli:index:#{r.get('edd-cli:index:current')}" }
+                  Sidekiq.redis { |r| "ember-cli:index:#{r.get('ember-cli:index:current')}" }
                 end
     index = Sidekiq.redis { |r| r.get(index_key) }
     render text: process_index(index), layout: false
   end
 
   private
-
-  def fetch_revision
-    rev = params[:revision]
-    rev if rev =~ SHORT_UUID_V4_REGEXP
-  end
 
   def process_index(index)
     return "INDEX NOT FOUND" unless index
